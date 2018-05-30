c9a5834b7466c7910fc915aec7eb21afbd244694
KseniaDr <kseniad@post.bgu.ac.il>
2018-05-31 00:30:52 +0300

    second push

--- pacman/src/pacman/Main.java
+++ pacman/src/pacman/Main.java
@@ -1,9 +1,12 @@
 package pacman;
 
+import menus.MainWindow;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new MainWindow();
+		//new SecondMenu();
 
 	}
 
